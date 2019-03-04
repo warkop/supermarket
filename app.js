a96e27eb0585edf5a057c8fe93ea8776325a2829
Mohammad Khafid Barokum <navigator619@gmail.com>
2019-03-04 22:48:57 +0700

    template engine

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const bodyParse = require('body-parser');
 const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.get('/favicon.ico', (req, res) => res.status(204));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParse.urlencoded({extended:false}));
-
-app.use('/admin',adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-// console.log(routes.tulisan)
 app.listen(3000);
