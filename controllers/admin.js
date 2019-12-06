b8cc2de54235733b03db99f5a9f1f8357ef6b471
Mohammad Khafid Barokum <navigator619@gmail.com>
2019-12-06 23:09:00 +0700

    course 108

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,9 +4,6 @@ exports.getAddProduct = (req, res, next) => {
     res.render('admin/add-product', {
       pageTitle: 'Add Product',
       path: '/admin/add-product',
-      formsCSS: true,
-      productCSS: true,
-      activeAddProduct: true
     });
 };
 
@@ -18,14 +15,11 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
 	Product.fetchAll(products => {
-    res.render('shop/product-list', {
+    res.render('admin/products', {
       prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
     });
   });
-    
 }
+
--- controllers/error.js
+++ controllers/error.js
@@ -1,11 +1,7 @@
 exports.get404 = (req, res, next) => {
     const path = req.url;
-    console.log(path);
-    if (path != '/' && path != '/admin/add-product') {
-        res.status(404).render('404', {
-            pageTitle: '404 Page',
-            path: path
-        });
-        return;
-    }
+    res.status(404).render('404', {
+        pageTitle: '404 Page',
+        path: '/404'
+    });
 };
