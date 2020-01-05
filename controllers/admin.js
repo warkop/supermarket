26edcd5b755b26ae60b179454c44554da6fbadab
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-01-05 22:51:38 +0700

    159

--- controllers/admin.js
+++ controllers/admin.js
@@ -82,6 +82,14 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProducts = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+  .then(product => {
+    return product.destroy();
+  })
+  .then(() => {
+    res.redirect('/admin/products');
+  })
+  .catch(err => {
+    console.log(err);
+  });
 }
