03870bdb22addcd9f6473a7016627cedcc157155
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-01-22 00:38:24 +0700

    setup mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,14 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'toor', {
-    dialect: 'mysql',
-    host: 'localhost'
-})
-
-module.exports = sequelize;
+const mongoConnect = callback => {
+    MongoClient.connect('mongodb+srv://moden:2udBQbrUWNyJIUkm@cluster0-8vr93.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(client => { 
+        console.log('Connected');
+        callback(client);
+    })
+    .catch(err => { 
+        console.log(err)
+    });
+}
+module.exports = mongoConnect;
